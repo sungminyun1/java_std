6b9d1bcccecb9f21cb6f7546877679525c1efea6
ysm <smy07207@gmail.com>
2023-03-07 01:31:53 +0900

    0307

---
+++ thisisjava/src/ch09/sec06/exam01/ButtonExample.java
@@ -0,0 +1,32 @@
+package ch09.sec06.exam01;
+
+public class ButtonExample {
+	public static void main(String[] args) {
+		Button btnOk = new Button();
+		
+		class OkListener implements Button.ClickListener {
+			@Override
+			public void onClick() {
+				System.out.println("Ok 버튼을 클릭했습니다.");
+			}
+		}
+		
+		btnOk.setClickListener(new OkListener());
+		
+		btnOk.click();
+		
+		
+		Button btnCancle = new Button();
+		
+		class CancleListener implements Button.ClickListener{
+			@Override
+			public void onClick() {
+				System.out.println("Cancle 버튼을 클릭했습니다");
+			}
+		}
+		
+		btnCancle.setClickListener(new CancleListener());
+		
+		btnCancle.click();
+	}
+}
